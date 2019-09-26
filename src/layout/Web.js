d35b900cc63e7bd90232cad5eed25c92cb3591f3
AUTOALERT\jason.rango <jason.rango@autoalert.com>
2019-09-26 10:26:08 -0500

    panels, set position

--- src/layout/Web.js
+++ src/layout/Web.js
@@ -6,11 +6,18 @@ class Web extends Component {
   render() {
     return (
       <div className="web-background">
-        <div>Nav Menu item</div>
         <div>
+          <ul>
+            <li><a className="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+          </ul>
+        </div>
+        <div className="left-panel">
           <span>left side navigation</span>
         </div>
-        <div>
+        <div className="right-panel">
           <Survivor />
         </div>
       </div>
